numerical errors fix; clipping large exponents, adding eps to avoid log(0), adding eps to avoid singular S matrix, checking condition number to ensure that S is well behaved

vimmjipda/code/tracking/trackers.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 
+import sys
 import numpy as np
 from vimmjipda.code.tracking import utilities
 
@@ -98,6 +99,9 @@ class VIMMJIPDATracker(Tracker):
         cluster.eta = np.zeros((n_t, m_k + 1))
         cluster.mu = np.zeros((n_t, M, m_k + 1))
         cluster.w = np.zeros((n_t, m_k + 1))
+        
+        max_exp = 25
+        eps = max(1e-10, sys.float_info.epsilon)
 
         for t, track in enumerate(cluster.tracks):
 
@@ -107,18 +111,21 @@ class VIMMJIPDATracker(Tracker):
             mode_probabilities = track.mode_probabilities
 
             existence_prob_miss = ((1 - P_D * track.visibility_probability) * track.existence_probability) / (
-                1 - P_D * track.visibility_probability * track.existence_probability
-            )
-            visibility_prob_miss = (1 - P_D) * track.visibility_probability / (1 - P_D * track.visibility_probability)
+                1 - P_D * track.visibility_probability * track.existence_probability + eps)
+            visibility_prob_miss = (1 - P_D) * track.visibility_probability / (1 - P_D * track.visibility_probability + eps)
 
             cluster.r[t] = np.hstack((np.ones(m_k), existence_prob_miss)).reshape(m_k + 1)
             cluster.eta[t] = np.hstack((np.ones(m_k), visibility_prob_miss)).reshape(m_k + 1)
             cluster.mu[t] = np.concatenate(
                 (
                     np.exp(
-                        np.log(mode_probabilities.reshape(M, 1))
-                        + np.log(measurement_likelihoods.reshape(M, m_k))
-                        - np.log(measurement_likelihoods_combined.reshape(1, m_k))
+                        np.clip(
+                            np.log(mode_probabilities.reshape(M, 1) + eps)
+                            + np.log(measurement_likelihoods.reshape(M, m_k) + eps) 
+                            - np.log(measurement_likelihoods_combined.reshape(1, m_k) + eps),
+                            a_min = None, 
+                            a_max = max_exp,
+                        )
                     ),
                     (mode_probabilities.reshape(M, 1).reshape(M, 1)),
                 ),
@@ -141,6 +148,9 @@ class VIMMJIPDATracker(Tracker):
         n_t = cluster.n_tracks
 
         marginal_association_probabilities = self.data_associator.get_marginal_association_probabilities(cluster)
+        
+        max_exp = 25
+        eps = max(1e-10, sys.float_info.epsilon)
 
         for t, track in enumerate(cluster.tracks):
             eta_t_j = cluster.eta[t]
@@ -154,36 +164,60 @@ class VIMMJIPDATracker(Tracker):
                 track.visibility_probability = 1
             else:
                 track.visibility_probability = np.exp(
-                    np.log(
-                        np.sum(
-                            np.exp(
-                                np.log(eta_t_j.reshape(m_k + 1))
-                                + np.ma.log(p_t_j.reshape(m_k + 1))
-                                + np.log(r_t_j.reshape(m_k + 1))
+                    np.clip(
+                        np.log(
+                            np.sum(
+                                np.exp(
+                                    np.clip(
+                                        np.log(eta_t_j.reshape(m_k + 1) + eps)
+                                        + np.ma.log(p_t_j.reshape(m_k + 1) + eps)
+                                        + np.log(r_t_j.reshape(m_k + 1) + eps),
+                                        a_min = None,
+                                        a_max = max_exp,
+                                    )
+                                )
                             )
+                        + eps
                         )
+                        - (np.log(track.existence_probability + eps)),
+                        a_min = None,
+                        a_max = max_exp,
                     )
-                    - (np.log(track.existence_probability))
                 )
 
             track.mode_probabilities = np.exp(
-                np.log(
-                    np.sum(
-                        np.exp(
-                            np.log(mu_t_s_j) + np.ma.log(p_t_j.reshape(1, m_k + 1)) + np.log(r_t_j.reshape(1, m_k + 1))
-                        ),
-                        axis=1,
+                np.clip(
+                    np.log(
+                        np.sum(
+                            np.exp(
+                                np.clip(
+                                    np.log(mu_t_s_j + eps)
+                                    + np.ma.log(p_t_j.reshape(1, m_k + 1) + eps)
+                                    + np.log(r_t_j.reshape(1, m_k + 1) + eps),
+                                    a_min = None,
+                                    a_max = max_exp,
+                                )
+                            ),
+                            axis=1,
+                        )
+                    + eps
                     )
+                    - (np.log(track.existence_probability + eps)),
+                    a_min = None,
+                    a_max = max_exp,
                 )
-                - (np.log(track.existence_probability))
             ).reshape(M)
 
             betas = (
                 np.exp(
-                    np.log(mu_t_s_j)
-                    + np.ma.log(p_t_j.reshape(1, m_k + 1))
-                    + np.log(r_t_j.reshape(1, m_k + 1))
-                    - (np.log(track.mode_probabilities.reshape(M, 1)) + np.log(track.existence_probability))
+                    np.clip(
+                        np.log(mu_t_s_j + eps)
+                        + np.ma.log(p_t_j.reshape(1, m_k + 1))
+                        + np.log(r_t_j.reshape(1, m_k + 1) + eps) 
+                        - (np.log(track.mode_probabilities.reshape(M, 1) + eps) + np.log(track.existence_probability + eps)),
+                        a_min = None,
+                        a_max = max_exp,
+                    )
                 )
                 .filled(0)
                 .reshape(M, m_k + 1)